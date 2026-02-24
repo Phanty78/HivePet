f0cf40ee6cb1eaddb8f45fb3c0b618314404ced8
Phanty78 <maeldonnart@hotmail.fr>
2026-02-25 00:10:46 +0100

    ADD - Implement configuration for database connection using environment variables in config.ts

---
+++ backend/src/config.ts
@@ -0,0 +1,11 @@
+import { env } from "bun";
+
+const DATABASE_URL = env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set");
+}
+
+export const config = {
+    databaseUrl: DATABASE_URL,
+};
