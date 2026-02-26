cfd7f5fd5f000b8cfcce553b8a252bc14d801b16
Phanty78 <maeldonnart@hotmail.fr>
2026-02-26 23:42:38 +0100

    ADD - ajout du parser

--- backend/src/blockchain/parser.ts
+++ backend/src/blockchain/parser.ts
@@ -1,1 +1,15 @@
-export async function parseBlock(block, currentBlock: number) {}
+import { SignedBlock } from "@hiveio/dhive";
+
+export async function parseBlock(block: SignedBlock, currentBlock: number) {
+    for (let i = 0; i < block.transactions.length; i++) {
+        const tx = block.transactions[i]
+        const txId = block.transaction_ids[i]
+        tx.operations.forEach((op) => {
+            if (op[0] === "custom_json" && op[1].id === "hivepet"){
+                const payload = JSON.parse(op[1].json)
+                const username = op[1].required_posting_auths[0]
+                console.log(`transaction de ${username} trouvé`)
+            }
+        })
+    }
+}
