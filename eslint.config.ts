d7466e914e7253ca415da51a7500ee830dfe8c3d
Phanty78 <maeldonnart@hotmail.fr>
2026-02-26 23:17:12 +0100

    ADD - ajout prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,18 +1,20 @@
-import eslint from "@eslint/js";
-import { defineConfig } from "eslint/config";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig(
-    eslint.configs.recommended,
-    tseslint.configs.strict,
-    tseslint.configs.stylistic,
-    {
-        rules: {
-            "max-lines": ["warn", { max: 1000, skipBlankLines: true, skipComments: true }],
-            "max-lines-per-function": ["warn", { max: 50, skipBlankLines: true, skipComments: true }],
-        },
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  eslintConfigPrettier,
+  {
+    rules: {
+      'max-lines': ['warn', { max: 1000, skipBlankLines: true, skipComments: true }],
+      'max-lines-per-function': ['warn', { max: 50, skipBlankLines: true, skipComments: true }],
     },
-    {
-        ignores: ["**/node_modules/", "**/dist/", "**/generated/"],
-    }
-);
+  },
+  {
+    ignores: ['**/node_modules/', '**/dist/', '**/generated/'],
+  }
+)
